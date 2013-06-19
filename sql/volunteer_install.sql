e72b1adc3a03e84760bbc48a512edecd9a5726cf
Dave Greenberg <dave@civicrm.org>
2013-06-19 10:14:48 -0700

    Added visibility_id to need table, changed role_id to int(10) since a need will only link to a single role.

--- sql/volunteer_install.sql
+++ sql/volunteer_install.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS civicrm_volunteer_need (
   id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Need Id',
   start_time datetime DEFAULT NULL,
-  duration decimal(20,2) DEFAULT NULL COMMENT 'if times are flexible then duration is probably more appropriate than an end_time field',
-  is_flexible tinyint(4) DEFAULT '0' COMMENT 'boolean indicating whether or not the time is flexible',
-  quantity int(11) DEFAULT NULL COMMENT 'the number of volunteers needed for this need in each time slot',
-  role_id varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Initially, this will be just a label (e.g., Lifeguard), but in future releases roles will have additional properties, such as qualifications.',
-  entity_id int(10) unsigned NOT NULL COMMENT 'Artificial FK to other CiviCRM entity.',
-  entity_table varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Entity table',
+  duration decimal(20,2) DEFAULT NULL COMMENT 'Length in minutes of this volunteer time slot.',
+  is_flexible tinyint(4) DEFAULT '0' COMMENT 'boolean indicating whether or not the time and role are flexible. Activities linked to a flexible need indicate that the volunteer is generally available.',
+  quantity int(11) DEFAULT NULL COMMENT 'Number of volunteers required for this need.',
+  visibility_id int(11) DEFAULT NULL COMMENT 'Implicit FK to option_value row in visibility option_group. Indicates whether this need is offered on public volunteer signup forms.',
+  role_id int(11) DEFAULT NULL COMMENT 'Implicit FK to option_value row in volunteer_role option_group.',
+  entity_id int(10) unsigned NOT NULL COMMENT 'Implicit FK to a CiviCRM entity which is the volunteer project (e.g. an event ID).',
+  entity_table varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Entity table for entity_id',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
