7edf47e276fd8c4476ab79560358bcf02af2e2f4
M Z Daryabeygi (ginkgomzd) <mdaryabeygi@web.confluencecorp.com>
2013-07-08 16:12:49 -0400

    fix required field spec

--- api/v3/Need.php
+++ api/v3/Need.php
@@ -57,7 +57,9 @@ function civicrm_api3_need_create($params) {
  * @param array $params array or parameters determined by getfields
  */
 function _civicrm_api3_need_create_spec(&$params) {
-  $params['title']['api.required'] = 1;
+  $params['project_id']['api.required'] = 1;
+  $params['is_flexible']['api.required'] = 1;
+  $params['is_active']['api.required'] = 1; /* TODO: this probably shouldn't be required */
 }
 
 /**
