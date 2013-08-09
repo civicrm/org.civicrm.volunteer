d356f183ef0266ac2972c6a1b53217859cb3ee40
Dave Greenberg <dave@civicrm.org>
2013-08-09 13:41:11 -0700

    Sort needs by start_time for signup form.

--- CRM/Volunteer/Form/VolunteerSignUp.php
+++ CRM/Volunteer/Form/VolunteerSignUp.php
@@ -288,6 +288,8 @@ class CRM_Volunteer_Form_VolunteerSignUp extends CRM_Core_Form {
       'version' => 3,
       'visibility_id' => CRM_Core_OptionGroup::getValue('visibility', 'public', 'name'),
     );
+    $params['options'] = array('sort' => 'start_time');
+
     $result = civicrm_api('VolunteerNeed', 'get', $params);
 
     if (CRM_Utils_Array::value('is_error', $result) === 0) {
