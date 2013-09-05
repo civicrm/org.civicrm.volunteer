2d1f39773e7f94f06197ade797ef4367a53846e4
Frank J. Gómez <fgomez@confluencecorp.com>
2013-09-05 16:05:41 -0400

    Remove is_flexible_form_value cruft (is_flexible checkbox is no longer displayed).

--- js/entities/needs.js
+++ js/entities/needs.js
@@ -7,7 +7,6 @@ CRM.volunteerApp.module('Entities', function(Entities, volunteerApp, Backbone, M
       'display_start_time': null, // generated in getNeeds
       'is_active' : 1,
       'is_flexible': 0,
-      'is_flexible_form_value': null,
       'duration': 0,
       'role_id': null,
       'start_time': null,
@@ -51,14 +50,6 @@ CRM.volunteerApp.module('Entities', function(Entities, volunteerApp, Backbone, M
           }
         });
         var needsCollection = new Entities.Needs(_.toArray(data.values));
-
-        for( index in needsCollection.models) {
-          need = needsCollection.models[index];
-          if (need.attributes.is_flexible == 1) {
-            need.attributes.is_flexible_form_value = 'checked';
-          }
-        }
-
         defer.resolve(needsCollection);
       }
 
