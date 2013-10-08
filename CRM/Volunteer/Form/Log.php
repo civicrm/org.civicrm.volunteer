9bcc9ea16bccd9212854cd2dfe4ed6e0d30e7d5a
Coleman Watts <coleman@civicrm.org>
2013-10-08 13:23:41 +0100

    VOL-20 - Use constant instead of value

--- CRM/Volunteer/Form/Log.php
+++ CRM/Volunteer/Form/Log.php
@@ -274,7 +274,7 @@ class CRM_Volunteer_Form_Log extends CRM_Core_Form {
 
     $statusMsg = ts('Volunteer hours have been recorded for %1 volunteers', array(1 => $count));
 
-    if (CRM_Utils_Array::value('snippet', $_REQUEST) == 6) {
+    if (CRM_Utils_Array::value('snippet', $_REQUEST) == CRM_Core_Smarty::PRINT_JSON) {
       CRM_Utils_System::civiExit(json_encode(array(
         'message' => $statusMsg,
       )));
