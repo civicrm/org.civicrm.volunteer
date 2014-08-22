74d1285669c87b7fe41739a5f61fb79211d144a5
Frank J. Gómez <frank@ginkgostreet.com>
2014-08-22 14:28:10 -0400

    VOL-71: Added permissions class to centralize permissions declarations and to override perms check function. Moved perms check from XML to preprocess function for volunteer sign-up form.

---
+++ CRM/Volunteer/Permission.php
@@ -0,0 +1,40 @@
+<?php
+
+class CRM_Volunteer_Permission extends CRM_Core_Permission {
+
+  /**
+   * Returns an array of permissions defined by this extension. Modeled off of
+   * CRM_Core_Permission::getCorePermissions().
+   *
+   * @return array Keyed by machine names with human-readable labels for values
+   */
+  public static function getVolunteerPermissions() {
+    $prefix = ts('CiviVolunteer', array('domain' => 'org.civicrm.volunteer')) . ': ';
+    return array(
+      'register to volunteer' => $prefix . ts('register to volunteer', array('domain' => 'org.civicrm.volunteer')),
+    );
+  }
+
+  /**
+   * Given a permission string or array, check for access requirements. For
+   * VOL-71, if this is a permissions-challenged Joomla instance, don't enforce
+   * CiviVolunteer-defined permissions.
+   *
+   * @param mixed $permissions The permission(s) to check as an array or string.
+   *        See parent class for examples.
+   * @return boolean
+   */
+  public static function check($permissions) {
+    $permissions = (array) $permissions;
+
+    if (CRM_Volunteer_Upgrader::isJoomlaPermsHackNeeded()) {
+      array_walk_recursive($permissions, function(&$v, $k) {
+        if (array_key_exists($v, self::getVolunteerPermissions())) {
+          $v = CRM_Core_Permission::ALWAYS_ALLOW_PERMISSION;
+        }
+      });
+    }
+
+    return parent::check($permissions);
+  }
+}
