646674ea4cfc60af12b8e3a5a526389494e10af7
Frank J. Gómez <frank@ginkgostreet.com>
2015-04-12 18:18:41 -0400

    VOL-51: Code clean-up. Reordered code to bring "start" listener to the top. Added a few comments. Removed unused "stop" listener.

--- js/apps/search/search_controller.js
+++ js/apps/search/search_controller.js
@@ -11,6 +11,40 @@ CRM.volunteerApp.module('Search', function(Search, volunteerApp, Backbone, Mario
     volunteerApp.searchRegion.show(layout);
   });
 
+  // Initialize entities and views
+  Search.on('start', function(params) {
+    Search.need_id = params.need_id;
+    Search.cnt_open_assignments = params.cnt_open_assignments;
+
+    // build search form
+    volunteerApp.Entities.getVolCustomFields().done(function(data) {
+      Search.formFields = new volunteerApp.Entities.VolCustomFields(data);
+      Search.formFields.push({
+        column_name: 'group',
+        html_type: 'Text',
+        label: 'Group'
+      });
+      Search.formView = new Search.fieldsCollectionView({
+        'collection': Search.formFields
+      });
+      layout.searchForm.show(Search.formView);
+    });
+
+    // build pager
+    Search.pagerData = new volunteerApp.Entities.ContactPagerModel();
+    Search.pager = new Search.pagerView({
+      'model': Search.pagerData
+    });
+    layout.searchPager.show(Search.pager);
+
+    // build search results listing
+    Search.results = new volunteerApp.Entities.Contacts();
+    Search.resultsView = new Search.resultsCompositeView({
+      'collection': Search.results
+    });
+    layout.searchResults.show(Search.resultsView);
+  });
+
   Search.saveAssignments = function(){
     var scheduledView = CRM.volunteerApp.module('Assign').scheduledView;
     var need = scheduledView.collection.get(Search.need_id);
@@ -44,41 +78,6 @@ CRM.volunteerApp.module('Search', function(Search, volunteerApp, Backbone, Mario
 
     doSave();
   };
-
-  // Initialize entities and views
-  Search.on('start', function(params) {
-    Search.need_id = params.need_id;
-    Search.cnt_open_assignments = params.cnt_open_assignments;
-
-    volunteerApp.Entities.getVolCustomFields().done(function(data) {
-      Search.formFields = new volunteerApp.Entities.VolCustomFields(data);
-      Search.formFields.push({
-        column_name: 'group',
-        html_type: 'Text',
-        label: 'Group'
-      });
-      Search.formView = new Search.fieldsCollectionView({
-        'collection': Search.formFields
-      });
-      layout.searchForm.show(Search.formView);
-    });
-
-    Search.pagerData = new volunteerApp.Entities.ContactPagerModel();
-    Search.pager = new Search.pagerView({
-      'model': Search.pagerData
-    });
-    layout.searchPager.show(Search.pager);
-
-    Search.results = new volunteerApp.Entities.Contacts();
-    Search.resultsView = new Search.resultsCompositeView({
-      'collection': Search.results
-    });
-    layout.searchResults.show(Search.resultsView);
-  });
-
-  // Detach event handlers
-  Search.on('stop', function() {
-  });
 
   Search.dialogSettings = {
     modal: true,
