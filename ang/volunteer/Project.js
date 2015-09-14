cf2ed493669ea99e62dc12cf78dbc72730bfdac6
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-09-13 17:34:47 -0700

    Added new fields to project edit page and connected them to new schema changes

--- ang/volunteer/Project.js
+++ ang/volunteer/Project.js
@@ -10,6 +10,18 @@
               id: $route.current.params.projectId
             });
           },
+          relationship_types: function(crmApi) {
+            return crmApi('OptionValue', 'get', {
+              "sequential": 1,
+              "option_group_id": "volunteer_project_relationship"
+            });
+          },
+          relationship_data: function(crmApi, $route) {
+            return crmApi('VolunteerProjectContact', 'get', {
+              "sequential": 1,
+              "project_id": $route.current.params.projectId
+            });
+          },
           is_entity: function() { return false; },
           profile_status: function(crmProfiles) {
             return crmProfiles.load();
@@ -37,15 +49,26 @@
   // The controller uses *injection*. This default injects a few things:
   //   $scope -- This is the set of variables shared between JS and HTML.
   //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  angular.module('volunteer').controller('VolunteerProject', function($scope, crmApi, crmStatus, crmUiHelp, crmProfiles, project, is_entity, profile_status) {
+  angular.module('volunteer').controller('VolunteerProject', function($scope, crmApi, crmStatus, crmUiHelp, crmProfiles, project, is_entity, profile_status, relationship_types, relationship_data) {
     // The ts() and hs() functions help load strings for this module.
     var ts = $scope.ts = CRM.ts('org.civicrm.volunteer');
-    var hs = $scope.hs = crmUiHelp({file: 'CRM/Volunteer/Volunteer'}); // See: templates/CRM/volunteer/Project.hlp
-
+    var hs = $scope.hs = crmUiHelp({file: 'CRM/Volunteer/Form/Volunteer'}); // See: templates/CRM/volunteer/Project.hlp
     project.profileCount = 0;
 
+
+    var relationships = {};
+    $(relationship_data.values).each(function(index, relationship) {
+      if (!relationships.hasOwnProperty(relationship.relationship_type_id)) {
+        relationships[relationship.relationship_type_id] = [];
+      }
+      relationships[relationship.relationship_type_id].push(relationship.contact_id);
+    });
+
+    $scope.relationships = relationships;
+    $scope.relationship_types = relationship_types.values;
     $scope.profile_status = profile_status;
     $scope.is_entity = is_entity;
+    project.is_active = (project.is_active === "1");
     $scope.project = project;
 
     $scope.addProfile = function() {
