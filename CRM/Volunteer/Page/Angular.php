af1a67e58ddfe3f7414ab8970930de6c33a58b96
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-03 14:36:41 -0800

    Removed unneeded run function

--- CRM/Volunteer/Page/Angular.php
+++ CRM/Volunteer/Page/Angular.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once 'Civi/Angular/Page/Main.php';
+require_once 'CRM/Volunteer/Angular/Manager.php';
 
 class CRM_Volunteer_Page_Angular extends Civi\Angular\Page\Main {
-  function run() {
-    parent::run();
+
+  public function __construct($title = NULL, $mode = NULL, $res = NULL) {
+    parent::__construct($title, $mode);
+    $this->angular = new Civi\Angular\VolunteerManager(\CRM_Core_Resources::singleton());
   }
 }
