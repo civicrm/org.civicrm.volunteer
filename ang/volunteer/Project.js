46986b62bbc435cfc54bbfb0c7326e9f57ba124f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-06 12:02:04 -0800

    Added permission based fallback to select2 from backbone widget

--- ang/volunteer/Project.js
+++ ang/volunteer/Project.js
@@ -99,6 +99,7 @@
     $scope.campaigns = campaigns;
     $scope.relationship_types = supporting_data.values.relationship_types;
     $scope.phone_types = supporting_data.values.phone_types;
+    $scope.supporting_data = supporting_data.values;
     $scope.profile_status = profile_status;
     project.is_active = (project.is_active == "1");
     $scope.project = project;
