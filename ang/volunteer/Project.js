ffdd80dcb9da021be2513dbfa295f4cdd6e07cde
Frank J. Gómez <frank@ginkgostreet.com>
2015-11-11 23:21:50 -0500

    Removed unused and recently-factored-out providers. Added missing provider.

--- ang/volunteer/Project.js
+++ ang/volunteer/Project.js
@@ -65,7 +65,7 @@
   );
 
 
-  angular.module('volunteer').controller('VolunteerProject', function($scope, $location, $q, crmApi, crmStatus, crmUiAlert, crmUiHelp, crmProfiles, countries, project, profile_status, campaigns, supporting_data, profiles, location_blocks, volBackbone) {
+  angular.module('volunteer').controller('VolunteerProject', function($scope, $location, $q, crmApi, crmUiAlert, crmUiHelp, countries, project, profile_status, campaigns, relationship_data, supporting_data, location_blocks, volBackbone) {
     // The ts() and hs() functions help load strings for this module.
     var ts = $scope.ts = CRM.ts('org.civicrm.volunteer');
     var hs = $scope.hs = crmUiHelp({file: 'CRM/Volunteer/Form/Volunteer'}); // See: templates/CRM/volunteer/Project.hlp
