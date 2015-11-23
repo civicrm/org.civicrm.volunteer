f519cb72b11fc8b186853357e63024a028d81adb
John Kirk <jkirk@ginkgostreet.com>
2015-11-23 17:22:49 +0000

    NYCCAH-48:responsive volunteer listings. Civix generated volunteer listings page

---
+++ CRM/Volunteer/Page/Listings.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_Volunteer_Page_Listings extends CRM_Core_Page {
+  function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(ts('Listings'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+}
