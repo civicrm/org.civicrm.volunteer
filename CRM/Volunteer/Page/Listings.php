198af30bdbbdb5e3ce9fd9445212b38ea3bee667
John Kirk <jkirk@ginkgostreet.com>
2015-11-23 17:50:13 +0000

    NYCCAH-48:responsive volunteer listings. Retrieve information for project and display it. No ordering yet.

--- CRM/Volunteer/Page/Listings.php
+++ CRM/Volunteer/Page/Listings.php
@@ -4,12 +4,45 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Volunteer_Page_Listings extends CRM_Core_Page {
   function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('Listings'));
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-
+    // Retrieve project id or bail.
+    $projectId = filter_input (INPUT_GET, 'project_id', FILTER_VALIDATE_INT);
+    
+    if (!$projectId) {
+      // Will fail $projectId is empty, invalid, or 0.
+      // TODO invalid project id.
+    }
+    
+    // Retrieve data and group it according to assignment time.
+    try {
+      $volunteerAssignments = civicrm_api3('VolunteerAssignment', 'get', array(
+        'sequential' => 1,
+        'project_id' => $projectId,
+        'count' => 0, // will not limit to first 25.
+      ));
+    } 
+    catch (Exception $e){
+      // TODO handle error retrieving assignments.
+      
+    }
+    
+    if ($volunteerAssignments['count'] == 0) {
+      $this->assign('errorMessage', 'No volunteers have been assigned to this project yet!'); // TODO include URL where to assign some.
+    }
+    
+    $this->assign('sortedResults', $this->sortVolunteerAssignments($volunteerAssignments['values']));
     parent::run();
+  }
+  
+  /**
+   * Sorts the volunteer assignments grouping them into timeslots.
+   * 
+   * @param array $volunteerAssignments - the values part from the output of the get api call.
+   * @return sortedResults
+   */
+  private function sortVolunteerAssignments($volunteerAssignments) {
+    $sortedResults = print_r($volunteerAssignments, TRUE);
+    
+    return $sortedResults;
   }
 }
