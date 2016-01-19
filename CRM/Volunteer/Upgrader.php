c23be9a4640884496beef347013016d45d72ba7f
Frank J. Gómez <frank@ginkgostreet.com>
2016-01-19 15:20:48 -0500

    Removed misleading code comment.

--- CRM/Volunteer/Upgrader.php
+++ CRM/Volunteer/Upgrader.php
@@ -116,7 +116,6 @@ class CRM_Volunteer_Upgrader extends CRM_Volunteer_Upgrader_Base {
     );
 
     foreach ($options as $opt) {
-      // Will throw exception if doesn't exist,
       $getOptionValues = civicrm_api3('OptionValue', 'get', $optionDefaults + $opt);
 
       // In the case of a user reinstalling CiviVolunteer we don't want duplicate options.
