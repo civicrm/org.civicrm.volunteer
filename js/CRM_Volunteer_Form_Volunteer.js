1e5c42da0f874790202f40c6eddd353810ae58a4
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-01-20 22:54:05 -0800

    Moved jquery hack to settings so it could be more easily removed

--- js/CRM_Volunteer_Form_Volunteer.js
+++ js/CRM_Volunteer_Form_Volunteer.js
@@ -1,8 +1,7 @@
 CRM.$(document).ready(function(event) {
-
-  CRM.origJQuery = window.jQuery; window.jQuery = CRM.$;
   
   if (CRM.VolunteerAngularSettings.Hash) {
     location.hash = CRM.VolunteerAngularSettings.Hash;
   }
+  
 });
