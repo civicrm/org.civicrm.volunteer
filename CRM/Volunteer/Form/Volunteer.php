473022b97066ad0b97061a5e3454546b23bfc234
Frank J. Gómez <frank@ginkgostreet.com>
2016-01-22 18:35:49 -0500

    Removed unused methods and members.

--- CRM/Volunteer/Form/Volunteer.php
+++ CRM/Volunteer/Form/Volunteer.php
@@ -52,13 +52,6 @@ class CRM_Volunteer_Form_Volunteer extends CRM_Event_Form_ManageEvent {
   private $loadAngular = FALSE;
 
   /**
-   * The profile IDs associated with this form
-   *
-   * @var array
-   */
-  private $_profile_ids = array();
-
-  /**
    * The project the form is acting on
    *
    * @var mixed CRM_Volunteer_BAO_Project if a project has been set, else boolean FALSE
@@ -85,35 +78,6 @@ class CRM_Volunteer_Form_Volunteer extends CRM_Event_Form_ManageEvent {
     return $this->_project;
   }
 
-  /***
-   * Save (or create a project)
-   */
-  protected function saveProject($params) {
-    $targetContacts = CRM_Utils_Array::value('target_contact_id', $params);
-    unset($params['target_contact_id']);
-
-    if (!empty($targetContacts)) {
-      $params['project_contacts']['volunteer_beneficiary'] = explode(',', $targetContacts);
-    }
-
-    $this->minimumProjectParams($params);
-    $this->_project = CRM_Volunteer_BAO_Project::create($params);
-    // if we created a project:
-    if (!array_key_exists('id', $params)) {
-      $form = $this->getSubmitValues();
-      if (CRM_Utils_Array::value('is_active', $form, 0) === '1') {
-        // create the flexible need
-        $need = array(
-          'project_id' => $this->_project->id,
-          'is_flexible' => '1',
-          'visibility_id' => CRM_Core_OptionGroup::getValue('visibility', 'public', 'name'),
-        );
-        CRM_Volunteer_BAO_Need::create($need);
-      }
-    }
-    return $this->_project;
-  }
-
   /**
    * Use Page properties to set default params for retrieving a project BAO
    *
@@ -133,33 +97,6 @@ class CRM_Volunteer_Form_Volunteer extends CRM_Event_Form_ManageEvent {
     if (!array_key_exists('entity_table', $params)) {
       $params['entity_table'] = CRM_Event_DAO_Event::$_tableName;
     }
-  }
-
-
-  /**
-   * Does a UFJoin lookup and caches it for future use.
-   *
-   * @return array of UFGroup (profile) IDs
-   */
-  private function getProfileIDs() {
-    if (empty($this->_profile_ids) && $this->getProject() !== FALSE) {
-      $dao = new CRM_Core_DAO_UFJoin();
-      $dao->entity_table = CRM_Volunteer_BAO_Project::$_tableName;
-      $dao->entity_id = $this->getProject()->id;
-      $dao->orderBy('weight asc');
-      $dao->find();
-      while ($dao->fetch()) {
-        $this->_profile_ids[] = $dao->uf_group_id;
-      }
-    }
-    if (empty($this->_profile_ids)) {
-      $this->_profile_ids[] = civicrm_api3('UFGroup', 'getvalue', array(
-        'name' => 'volunteer_sign_up',
-        'return' => 'id',
-      ));
-    }
-
-    return $this->_profile_ids;
   }
 
   /**
@@ -227,63 +164,4 @@ class CRM_Volunteer_Form_Volunteer extends CRM_Event_Form_ManageEvent {
     }
   }
 
-
-  /**
-   * Associates user-selected profiles with the volunteer project
-   *
-   * @param array $profiles
-   */
-  function saveProfileSelections($profiles) {
-    // first delete all past entries
-    $params = array(
-      'entity_table' => CRM_Volunteer_BAO_Project::$_tableName,
-      'entity_id' => $this->_project->id,
-    );
-    CRM_Core_BAO_UFJoin::deleteAll($params);
-
-    if (empty($profiles)) {
-      $profiles[] = civicrm_api3('UFGroup', 'getvalue', array(
-        'name' => 'volunteer_sign_up',
-        'return' => 'id',
-      ));
-    }
-
-    // store the new selections
-    foreach($profiles as $key => $profile_id) {
-      CRM_Core_BAO_UFJoin::create(array(
-        'entity_id' => $this->_project->id,
-        'entity_table' => CRM_Volunteer_BAO_Project::$_tableName,
-        'is_active' => 1,
-        'module' => 'CiviVolunteer',
-        'uf_group_id' => $profile_id,
-        'weight' => $key,
-      ));
-    }
-  }
-
-  static function validateProfileForDedupe($profileIds) {
-    $cantDedupe = false;
-    switch (CRM_Event_Form_ManageEvent_Registration::canProfilesDedupe($profileIds, 0)) {
-      case 0:
-        $dedupeTitle = ts('Duplicate Matching Impossible', array('domain' => 'org.civicrm.volunteer'));
-        $cantDedupe = ts('The selected profiles do not contain the fields necessary to match volunteer sign ups with existing contacts.  This means all anonymous sign ups will result in a new contact.', array('domain' => 'org.civicrm.volunteer'));
-        break;
-      case 1:
-        $dedupeTitle = 'Duplicate Contacts Possible';
-        $cantDedupe = ts('The selected profiles can collect enough information to match sign ups with existing contacts, but not all of the relevant fields are required.  Anonymous sign ups may result in duplicate contacts.', array('domain' => 'org.civicrm.volunteer'));
-    }
-    if ($cantDedupe) {
-      CRM_Core_Session::setStatus($cantDedupe, $dedupeTitle, 'alert dedupenotify', array('expires' => 0));
-    }
-  }
-
-  /**
-   * Return a descriptive name for the page, used in wizard header
-   *
-   * @return string
-   * @access public
-   */
-  public function getTitle() {
-    return ts('Volunteers', array('domain' => 'org.civicrm.volunteer'));
-  }
 }
