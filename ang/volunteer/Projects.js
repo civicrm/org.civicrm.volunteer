45750b11fd50e305cfff94294a4827152911b52a
Frank J. Gómez <frank@ginkgostreet.com>
2016-01-25 13:45:45 -0500

    Cleaned up boilerplate.

--- ang/volunteer/Projects.js
+++ ang/volunteer/Projects.js
@@ -24,18 +24,10 @@
     }
   );
 
-  // The controller uses *injection*. This default injects a few things:
-  //   $scope -- This is the set of variables shared between JS and HTML.
-  //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  //   myContact -- The current contact, defined above in config().
-  angular.module('volunteer').controller('VolunteerProjects', function($scope, crmApi, crmStatus, crmUiHelp, projectData, $location, volunteerBackbone, campaigns, $window) {
+  angular.module('volunteer').controller('VolunteerProjects', function ($scope, crmApi, crmStatus, crmUiHelp, projectData, $location, volunteerBackbone, campaigns, $window) {
     // The ts() and hs() functions help load strings for this module.
     var ts = $scope.ts = CRM.ts('volunteer');
     var hs = $scope.hs = crmUiHelp({file: 'CRM/volunteer/Projects'}); // See: templates/CRM/volunteer/Projects.hlp
-
-    // We have myContact available in JS. We also want to reference it in HTML.
-
-
 
     $scope.searchParams = {};
     $scope.projects = projectData.values;
