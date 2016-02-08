ca6e296af861170862a3624fe21ebbc26c4e2169
Frank J. Gómez <frank@ginkgostreet.com>
2016-02-07 19:01:05 -0500

    Utilize permission check in the menu layer instead of the form layer.

--- CRM/Volunteer/Form/VolunteerSignUp.php
+++ CRM/Volunteer/Form/VolunteerSignUp.php
@@ -146,16 +146,7 @@ class CRM_Volunteer_Form_VolunteerSignUp extends CRM_Core_Form {
    * @access public
    */
   function preProcess() {
-    
     $this->redirectLegacyRequests();
-    
-    // VOL-71: permissions check is moved from XML to preProcess function to support
-    // permissions-challenged Joomla instances
-    if (CRM_Core_Config::singleton()->userPermissionClass->isModulePermissionSupported()
-      && !CRM_Volunteer_Permission::check('register to volunteer')
-    ) {
-      CRM_Utils_System::permissionDenied();
-    }
 
     $validNeedIds = array();
     $needs = CRM_Utils_Request::retrieve('needs', 'String', $this, TRUE);
