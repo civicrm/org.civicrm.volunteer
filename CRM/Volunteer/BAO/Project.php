bba319e81a89f34fd33752d343ba24de97f27e7a
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-04-08 21:45:26 -0700

    Removed unneeded wrapper to Volunteer/BAO/Project::get_open_needs()

--- CRM/Volunteer/BAO/Project.php
+++ CRM/Volunteer/BAO/Project.php
@@ -757,15 +757,7 @@ class CRM_Volunteer_BAO_Project extends CRM_Volunteer_DAO_Project {
     return $this->roles;
   }
 
-  /**
-   * Public wrapper around _get_open_needs()
-   * 
-   * @return array
-   */
-  public function get_open_needs() {
-    return $this->_get_open_needs();
-  }
-
+  
   /**
    * Sets and returns $this->open_needs. Delegate of __get().
    *
