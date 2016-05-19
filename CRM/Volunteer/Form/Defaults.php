d894c7adb4b6831fa93429c205ae5fad2b8a1737
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-05-18 17:34:36 -0700

    Added empty form for Defaults

---
+++ CRM/Volunteer/Form/Defaults.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+/**
+* Form controller class
+*
+* @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+*/
+class CRM_Volunteer_Form_IncludeProfile extends CRM_Core_Form {
+
+}
