de1f72ce9609c0c684fdca504c5999c370d032f6
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-05-18 17:40:17 -0700

    Fixed paste error in class name

--- CRM/Volunteer/Form/Defaults.php
+++ CRM/Volunteer/Form/Defaults.php
@@ -7,6 +7,6 @@ require_once 'CRM/Core/Form.php';
 *
 * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
 */
-class CRM_Volunteer_Form_IncludeProfile extends CRM_Core_Form {
+class CRM_Volunteer_Form_Defaults extends CRM_Core_Form {
 
 }
