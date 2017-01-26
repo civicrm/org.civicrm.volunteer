1ee3634dc6664c03f69adbb46dfe7a68698b3cf7
Frank J. Gómez <frank@ginkgostreet.com>
2017-01-26 14:37:49 -0500

    VOL-283: Added animation to row removal.

--- js/CRM_Volunteer_Form_Log.js
+++ js/CRM_Volunteer_Form_Log.js
@@ -9,6 +9,11 @@ CRM.$(function($) {
   // Otherwise, the "added" row could show up anywhere in the table rather than
   // at the bottom as expected.
   $('.crm-vol-remove-row').click(function(e) {
-    $(this).closest('.crm-grid-row').remove();
+    e.preventDefault();
+    var row = $(this).closest('.crm-grid-row');
+    // Animation is applied to children because elemnents with display:table don't slideUp.
+    row.find('.crm-grid-cell').slideUp(100, function(){
+      row.remove();
+    });
   });
 });
