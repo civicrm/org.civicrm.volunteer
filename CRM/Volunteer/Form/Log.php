48c77b32b8a1e91196d1bbfc7ce3e483b5ee2127
Frank J. Gómez <frank@ginkgostreet.com>
2017-01-26 13:44:36 -0500

    Removed outdated hack for including resources, now the asset management framework fully supports pop-ups.

--- CRM/Volunteer/Form/Log.php
+++ CRM/Volunteer/Form/Log.php
@@ -84,12 +84,10 @@ class CRM_Volunteer_Form_Log extends CRM_Core_Form {
       $this->_title .= ' )';
     }
 
-    /*
-     * Because CiviCRM's asset management framework isn't mature yet (e.g., adding
-     * assets to forms rendered in pop-ups using CRM_Core_Resources doesn't work),
-     * we pass a URL fragment to the template and include them via HTML.
-     */
-    $this->assign('extResourceURL', CRM_Core_Resources::singleton()->getUrl('org.civicrm.volunteer'));
+    CRM_Core_Resources::singleton()
+        ->addStyleFile('org.civicrm.volunteer', 'css/commendation.css')
+        ->addScriptFile('org.civicrm.volunteer', 'js/commendation.js');
+
     $this->assign('vid', $this->_vid);
   }
 
