431cf612d9870e9952f41cde302a6c7ba3827e5d
Frank J. Gómez <frank@ginkgostreet.com>
2017-01-26 14:10:44 -0500

    VOL-283: Added ability to remove entries from the Log Hours UI.

--- js/CRM_Volunteer_Form_Log.js
+++ js/CRM_Volunteer_Form_Log.js
@@ -3,4 +3,12 @@ CRM.$(function($) {
     $('div.hiddenElement:first').show().removeClass('hiddenElement').addClass('crm-grid-row').css('display', 'table-row');
     e.preventDefault();
   });
+
+  // Add ability to remove a row. Because "adding" just unhides the first hidden
+  // row, it is more sensible to remove the row rather than clear and hide it.
+  // Otherwise, the "added" row could show up anywhere in the table rather than
+  // at the bottom as expected.
+  $('.crm-vol-remove-row').click(function(e) {
+    $(this).closest('.crm-grid-row').remove();
+  });
 });
