88141c32f41c8ac9461ffa21e82cb57299386920
David Hayes <david@blackbrick.software>
2019-09-24 14:08:29 -0500

    remove console .log

--- ang/volunteer.js
+++ ang/volunteer.js
@@ -241,7 +241,6 @@
               CRM
                 .loadForm(url, settings)
                 .on('crmFormSuccess', function(e, data) {
-                  console.log('success', data);
                   scope.refreshCustomData();
                 });
             });
